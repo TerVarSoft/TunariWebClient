a85727b900d95b0811d1962bbeda19306a9d3c5c
Mirko Terrazas <mirko.jterrazasv@gmail.com>
2016-12-27 01:40:48 -0400

    set up server

---
+++ mainProd.js
@@ -0,0 +1,7 @@
+var app = require('./app');
+
+var port = process.env.PORT || 5000;
+
+var server = app.listen(port, function () {
+  console.log('Tunari Web client is running at development mode.', port);
+});
