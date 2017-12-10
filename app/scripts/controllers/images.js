9c890710080c6ab223ba48d49167403a841bb188
Mirko Terrazas <mirko.jterrazasv@gmail.com>
2017-12-10 00:08:30 -0400

    Move image tester to use cloudinary

--- app/scripts/controllers/images.js
+++ app/scripts/controllers/images.js
@@ -8,60 +8,47 @@
  * Controller of the tunariApp
  */
 angular.module('tunariApp')
-  .controller('ImagesCtrl', ['$scope', '$http', '$mdDialog', 'AuthToken', 'Products', 'ProductInfo', 
-    function ($scope, $http, $mdDialog, AuthToken, Products, ProductInfo) {
+  .controller('ImagesCtrl', ['$scope', '$http', '$mdDialog', 'Products', 'ProductInfo', 
+    function ($scope, $http, $mdDialog, Products, ProductInfo) {
     
     $scope.layout.title = 'Monitor de Imagenes'; 
     $scope.layout.hideHeader = false;
 
-    $scope.smallImagesIndicator = 0;
-    $scope.smallLoaderIsDeterminateMode = true;
-    $scope.isSmallButtonDisabled = false;
-    $scope.noSmallImageProducts = [];
+    $scope.imagesIndicator = 0;
+    $scope.loaderIsDeterminateMode = true;
+    $scope.isTestButtonDisabled = false;
+    $scope.noImageProducts = [];
 
-    $scope.mediumImagesIndicator = 0;
-    $scope.mediumLoaderIsDeterminateMode = true;
-    $scope.isMediumButtonDisabled = false;
-    $scope.noMediumImageProducts = [];
+    $scope.testImages = function() {
+        $scope.imagesIndicator = 0;
+        $scope.loaderIsDeterminateMode = false;
+        $scope.isTestButtonDisabled = true;
 
-    $scope.largeImagesIndicator = 0;
-    $scope.largeLoaderIsDeterminateMode = true;
-    $scope.isLargeButtonDisabled = false;
-    $scope.noLargeImageProducts = [];
-
-    
-
-    $scope.testSmallImages = function() {
-        $scope.smallImagesIndicator = 0;
-        $scope.smallLoaderIsDeterminateMode = false;
-        $scope.isSmallButtonDisabled = true;
-
-        $scope.noSmallImageProducts = [];
+        $scope.noImageProducts = [];
 
         Products.getList({properties: 'name category properties', queryLimit: 1000}).then(function(products) {
             var step = 100 /products.length;
             _.each(products, function(product, index) {
 
-                $http.get(ProductInfo.getProductImageUrl(product, "-S"), {
+                $http.get(product.imageUrl, {
                   responseType: 'arraybuffer',
                   headers: {
                     'accept': 'image/webp,image/*,*/*;q=0.8',
-                    'authorization': 'Bearer ' + AuthToken.getToken()
                   }
                 }).then(function() {
-                    $scope.smallLoaderIsDeterminateMode = true;
-                    $scope.smallImagesIndicator += step;
+                    $scope.loaderIsDeterminateMode = true;
+                    $scope.imagesIndicator += step;
 
                     if (index === products.length - 1){ 
-                        $scope.isSmallButtonDisabled = false;
+                        $scope.isTestButtonDisabled = false;
                     }         
                 }, function() {
-                    $scope.smallLoaderIsDeterminateMode = true;
-                    $scope.smallImagesIndicator += step;
-                    $scope.noSmallImageProducts.push(product);
+                    $scope.loaderIsDeterminateMode = true;
+                    $scope.imagesIndicator += step;
+                    $scope.noImageProducts.push(product);
 
                     if (index === products.length - 1){ 
-                        $scope.isSmallButtonDisabled = false;
+                        $scope.isTestButtonDisabled = false;
                     } 
                   });   
             });
@@ -69,147 +56,18 @@ angular.module('tunariApp')
         });
     }
 
-
-    $scope.testMediumImages = function() {
-        $scope.mediumImagesIndicator = 0;
-        $scope.mediumLoaderIsDeterminateMode = false;
-        $scope.isMediumButtonDisabled = true;
-
-        $scope.noMediumImageProducts = [];
-
-        Products.getList({properties: 'name category properties', queryLimit: 1000}).then(function(products) {
-            var step = 100 /products.length;
-            _.each(products, function(product, index) {
-
-                $http.get(ProductInfo.getProductImageUrl(product, "-M"), {
-                  responseType: 'arraybuffer',
-                  headers: {
-                    'accept': 'image/webp,image/*,*/*;q=0.8',
-                    'authorization': 'Bearer ' + AuthToken.getToken()
-                  }
-                }).then(function() {
-                    $scope.mediumLoaderIsDeterminateMode = true;
-                    $scope.mediumImagesIndicator += step;
-
-                    if (index === products.length - 1){ 
-                        $scope.isMediumButtonDisabled = false;
-                    }         
-                }, function() {
-                    $scope.mediumLoaderIsDeterminateMode = true;
-                    $scope.mediumImagesIndicator += step;
-                    $scope.noMediumImageProducts.push(product);
-
-                    if (index === products.length - 1){ 
-                        $scope.isMediumButtonDisabled = false;
-                    } 
-                  });   
-            });
-
-        });
-    }
-
-    $scope.testLargeImages = function() {
-        $scope.largeImagesIndicator = 0;
-        $scope.largeLoaderIsDeterminateMode = false;
-        $scope.isLargeButtonDisabled = true;
-
-        $scope.noLargeImageProducts = [];
-
-        Products.getList({properties: 'name category properties', queryLimit: 1000}).then(function(products) {
-            var step = 100 /products.length;
-            _.each(products, function(product, index) {
-
-                $http.get(ProductInfo.getProductImageUrl(product, "-L"), {
-                  responseType: 'arraybuffer',
-                  headers: {
-                    'accept': 'image/webp,image/*,*/*;q=0.8',
-                    'authorization': 'Bearer ' + AuthToken.getToken()
-                  }
-                }).then(function() {
-                    $scope.largeLoaderIsDeterminateMode = true;
-                    $scope.largeImagesIndicator += step;
-
-                    if (index === products.length - 1){ 
-                        $scope.isLargeButtonDisabled = false;
-                    }         
-                }, function() {
-                    $scope.largeLoaderIsDeterminateMode = true;
-                    $scope.largeImagesIndicator += step;
-                    $scope.noLargeImageProducts.push(product);
-
-                    if (index === products.length - 1){ 
-                        $scope.isLargeButtonDisabled = false;
-                    } 
-                  });   
-            });
-
-        });
-    }
-
-    $scope.showSmallResults = function() {
+    $scope.showResults = function() {
       $mdDialog.show({
             controller: 'NoImageProductsListCtrl',
             templateUrl: '../../views/modal/noImageProductsList.html',
             parent: angular.element(document.body),
             locals: {
-                products: $scope.noSmallImageProducts,
-                title: "Productos sin imagenes pequeñas"
+                products: $scope.noImageProducts,
+                title: "Productos sin imagenes"
             },
             targetEvent: event,
             clickOutsideToClose: true
         }).then(function() {
         }, function() {});  
-    }
-
-    $scope.showMediumResults = function() {
-      $mdDialog.show({
-            controller: 'NoImageProductsListCtrl',
-            templateUrl: '../../views/modal/noImageProductsList.html',
-            parent: angular.element(document.body),
-            locals: {
-                products: $scope.noMediumImageProducts,
-                title: "Productos sin imagene medianas"
-            },
-            targetEvent: event,
-            clickOutsideToClose: true
-        }).then(function() {
-        }, function() {});
-    }
-    
-    $scope.showLargeResults = function() {
-      $mdDialog.show({
-            controller: 'NoImageProductsListCtrl',
-            templateUrl: '../../views/modal/noImageProductsList.html',
-            parent: angular.element(document.body),
-            locals: {
-                products: $scope.noLargeImageProducts,
-                title: "Productos sin imagenes grandes"
-            },
-            targetEvent: event,
-            clickOutsideToClose: true
-        }).then(function() {
-        }, function() {});
-    }
-
-    $scope.showProductsWithouthAnyImage = function() {
-
-        var productsWithouthAnyImage = 
-            _.intersectionBy($scope.noSmallImageProducts, $scope.noMediumImageProducts, 'name');
-
-        var productsWithouthAnyImage = 
-            _.intersectionBy(productsWithouthAnyImage, $scope.noLargeImageProducts, 'name');
-
-        $mdDialog.show({
-                controller: 'NoImageProductsListCtrl',
-                templateUrl: '../../views/modal/noImageProductsList.html',
-                parent: angular.element(document.body),
-                locals: {
-                    products: productsWithouthAnyImage,
-                    title: "Productos sin ninguna imagen"
-                },
-                targetEvent: event,
-                clickOutsideToClose: true
-            }).then(function() {
-            }, function() {});
     }
   }]);
